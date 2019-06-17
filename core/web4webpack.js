65b4fd5fdd4df013665908bc0523f33c6b46d8c7
famibee <famibee@gmail.com>
2019-06-17 21:37:18 +0900

    SKYNovel v0.5.4 更新、インタフェイス変更対応

--- core/web4webpack.js
+++ core/web4webpack.js
@@ -6,4 +6,4 @@ const h = require('./plugin.js').default;
 for (const nm in h) hPlg[nm] = require(`./plugin/${nm}/index.js`);
 
 const SysWeb_1 = require('skynovel/core/lib/sn/SysWeb');
-new SysWeb_1.SysWeb(hPlg, 'prj/top/');
+new SysWeb_1.SysWeb(hPlg, {cur: 'prj/top/'});
