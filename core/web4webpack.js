f7466fd6639aca980dd2c3d7bda6475bbe4eaafa
famibee <famibee@gmail.com>
2020-02-06 04:24:34 +0900

    追い更新6

--- core/web4webpack.js
+++ core/web4webpack.js
@@ -2,21 +2,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 
-console.log(`fn:web4webpack.js line:5 `);
 const hPlg = {};
 const h = require('./plugin.js').default;
 for (const nm in h) hPlg[nm] = require(`./plugin/${nm}/index.js`);
 
-console.log(`fn:web4webpack.js line:10 `);
 const {SysWeb} = require('skynovel/core/lib/sn/SysWeb');
-console.log(`fn:web4webpack.js line:12 `);
 //@ts-ignore
 const sys = new SysWeb(hPlg, {cur: 'prj/top/', dip: '{"expanding": false}'});
 //const sys = new SysWeb(hPlg, {cur: 'prj/top/'});
-console.log(`fn:web4webpack.js line:16 `);
 globalThis.runSN = (cur = 'top')=> sys.run(cur);
 globalThis.stopSN = ()=> sys.stop();
-console.log(`fn:web4webpack.js line:19 `);
+
 
 /*
 const init = async ()=> {
