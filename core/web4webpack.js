bcd6cc56af4d576e6edead6ddbe9a65c6a95fc6c
famibee <famibee@gmail.com>
2020-02-10 17:30:06 +0900

    追い更新2

--- core/web4webpack.js
+++ core/web4webpack.js
@@ -7,32 +7,9 @@ const h = require('./plugin.js').default;
 for (const nm in h) hPlg[nm] = require(`./plugin/${nm}/index.js`);
 
 const {SysWeb} = require('skynovel/core/lib/sn/SysWeb');
+const dip = {'expanding': false};
+if (location.pathname == '/index_app.html') dip['oninit_run'] = false;
 //@ts-ignore
-const sys = new SysWeb(hPlg, {cur: 'prj/top/', dip: '{"expanding": false}'});
-//const sys = new SysWeb(hPlg, {cur: 'prj/top/'});
+const sys = new SysWeb(hPlg, {cur: 'prj/top/', dip: JSON.stringify(dip)});
 globalThis.runSN = (cur = 'top')=> sys.run(cur);
 globalThis.stopSN = ()=> sys.stop();
-
-
-/*
-const init = async ()=> {
-	const hPlg = {};
-//	const h = require('./plugin.js').default;
-	const {default: h} = await import('./plugin.js');
-//	for (const nm in h) hPlg[nm] = require(`./plugin/${nm}/index.js`);
-	for (const nm in h) {
-//		hPlg[nm] = require(`./plugin/${nm}/index.js`);
-		hPlg[nm] = await import(`./plugin/${nm}/index.js`);
-	}
-
-	const {SysWeb} = require('skynovel/core/lib/sn/SysWeb');
-//	const {SysWeb} = await import('skynovel/core/lib/sn/SysWeb');
-	const sys = new SysWeb(hPlg, {cur: 'prj/top/', dip: '{"expanding": false}'});
-	//const sys = new SysWeb_1.SysWeb(hPlg, {cur: 'prj/top/'});
-	window.runSN = (cur = 'top')=> sys.run(cur);
-	window.stopSN = ()=> sys.stop();
-
-console.log(`fn:web4webpack.js line:32 Ok.`);
-};
-init();
-*/
