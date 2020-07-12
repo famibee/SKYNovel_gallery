9e8bfd68d58fba64c2be6b535a44686949291988
famibee <famibee@gmail.com>
2020-07-13 07:37:29 +0900

    v2.0.8：upd：【履歴と機能追加】サンプルで、Enterキーなどで本文を読み進めさせないように

--- docs/sw.js
+++ docs/sw.js
@@ -114,7 +114,7 @@ define("./sw.js",['./workbox-00ef8af7'], function (workbox) { 'use strict';
     "revision": "16d232855d6ef994591238441c4ae7b1"
   }, {
     "url": "prj/web.vendor.js",
-    "revision": "f2fff81c5075039cd9eb562faedad40f"
+    "revision": "a8d0cd2323c65a60bb5eb814d36f93a6"
   }], {
     "directoryIndex": "/"
   });
